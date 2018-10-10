9e1c62e472aae7399f4afb95171ed39c3b7ca2ef
pbchun <pbchun12@gmail.com>
2018-10-10 11:30:58 -0600

    initial commit with basic todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: 'Hello Coding Garden!',
+      newTodo: '',
+      todos: [{
+        title: 'Learn React',
+        done: false
+      }, {
+        title: 'Learn JSX',
+        done: false
+      }]
+    };
+  }
+
+  newTodoChange(event) {
+    this.setState({
+      newTodo: event.target.value
+    })
+  }
+
+  formSubmitted(event) {
+    event.preventDefault();
+    // **Optional way of using a const**
+    // const todos = this.state.todos.slice();
+    // todos.push({
+    //   title: this.state.newTodo,
+    //   done: false
+    // })
+    this.setState({
+      newTodo: '',
+      todos: [...this.state.todos, {
+        title: this.state.newTodo,
+        done: false
+      }]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h3>{this.state.message}</h3>
+        <form onSubmit={(event) => this.formSubmitted(event)}>
+          <label htmlFor="newTodo">New Task</label>
+          <input onChange={(event) => this.newTodoChange(event)} id="newTodo" name="newTodo" value={this.state.newTodo} />
+          <button type="submit">Add Task</button>
+        </form>
+        <ul>
+          {this.state.todos.map(todo => {
+            return <li key={todo.title}>{todo.title}</li>
+          })}
+        </ul>
       </div>
     );
   }
