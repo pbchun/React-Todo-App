f875063595a9f23024983d290287e0cb6ae38f5c
pbchun <pbchun12@gmail.com>
2018-10-31 08:58:36 -0600

    changed from todo to upcoming event list

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      message: 'Hi Peter!',
+      message: "Peter's Upcoming Events",
       newTodo: '',
       todos: [{
-        title: 'Plan Trip',
+        title: 'Cherub',
+        date: '11/3/18',
+        location: 'Ogden Theater',
         done: false
       }, {
         title: 'Book Hotel',
         done: false
       }, {
         title: 'Book Flights',
+        done: false
+      }, {
+        title: 'Book Rental Car',
         done: false
       }]
     };
@@ -65,20 +70,24 @@ class App extends Component {
       <div className="App">
         <h3>{this.state.message}</h3>
         <form onSubmit={(event) => this.formSubmitted(event)}>
-          <label htmlFor="newTodo">New Task</label>
+          <label htmlFor="newTodo">Add A New Event</label>
           <input onChange={(event) => this.newTodoChange(event)} id="newTodo" name="newTodo" value={this.state.newTodo} />
-          <button type="submit">Add Task</button>
+          <button type="submit">Add Event</button>
         </form>
         <ul>
           {this.state.todos.map((todo, index) => {
-            return (<li key={todo.title}>
-            <input onChange={(event) => this.toggleTodoDone(event, index)} type="checkbox" />
-            {/* <span style={{
-              textDecoration: todo.done ? 'line-through' : 'inherit'
-            }}>{todo.title}</span> */}
-            <span className={todo.done ? 'done' : ''}>{todo.title}</span>
-            <button className="remove-btn" onClick={() => this.removeTodo(index)}>Remove</button>
-            </li>)
+            return (
+              <li key={todo.title}>
+              <input onChange={(event) => this.toggleTodoDone(event, index)} type="checkbox" />
+              {/* <span style={{
+                textDecoration: todo.done ? 'line-through' : 'inherit'
+              }}>{todo.title}</span> */}
+              <h3 className={todo.done ? 'done' : ''}>{todo.title}</h3>
+              <h5 className="event-date">{todo.date}</h5>
+              <h4 className="event-location">{todo.location}</h4>
+              <button className="remove-btn" onClick={() => this.removeTodo(index)}>Remove</button>
+              </li>
+            )
           })}
         </ul>
       </div>
